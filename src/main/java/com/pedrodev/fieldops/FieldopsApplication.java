905a33ea08d8e81fab99bf65cb8de218b0256cbf
Pedro Soares <asp.pedro30@gmail.com>
2026-05-13 00:47:01 -0300

    first commit

---
+++ src/main/java/com/pedrodev/fieldops/FieldopsApplication.java
@@ -0,0 +1,13 @@
+package com.pedrodev.fieldops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FieldopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrodev/fieldops/FieldopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrodev.fieldops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
